621ba55c59992df9d12ee6ce9e0abf7c829eec73
lucianonomade <119142532+lucianonomade@users.noreply.github.com>
2026-04-12 23:14:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: ":all:",
     port: 8080,
     hmr: {
       overlay: false,

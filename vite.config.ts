43e8270965930b1c8e8d0489b2c6d353f829da6d
lucianonomade <119142532+lucianonomade@users.noreply.github.com>
2026-04-12 23:16:44 -0300

    host

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: ":all:",
+    host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
   preview: {
-    allowedHosts: "all",
+    host: "0.0.0.0",
+    allowedHosts: ["all", "index-sistemamod.ps261p.easypanel.host"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
